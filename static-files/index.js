0a6f3e8dd40dbf852ef1816e8ad838ad667ed07c
chaichunyan <chaichunyan@kingsoft.com>
2016-06-29 18:37:24 +0800

    update static files

--- static-files/index.js
+++ static-files/index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use('/public', express.static('public'));
 
-app.use('/static', express.static('static'));
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(3000, function () {
   console.log('static files app listening on port 3000!');
